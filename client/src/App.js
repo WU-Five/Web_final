2f623c4c44247bbeeaf4c9d75af5b08e99507b4b
KUAN-HSUN-LI <b06209027@ntu.edu.tw>
2019-06-22 23:03:17 +0800

    add introduction

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import TopTabs from './component/TopTabs';
 import Login from './component/Login';
 import Register from './component/Register';
-import { Switch, Route } from 'react-router-dom';
-
+import Introduction from './component/Introduction';
+import { Switch, Route, Redirect } from 'react-router-dom';
 function App() {
 	return (
 		<div className="App">
 			<TopTabs />
 			<Switch>
+				<Route exact path="/" render={() => <Redirect to="/introduction" />} />
+				<Route path="/introduction" component={Introduction} />
 				<Route path="/login" component={Login} />
 				<Route path="/register" component={Register} />
 			</Switch>
---
+++ client/src/component/Introduction.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Introduction = () => {
+	return (
+		<div>
+			<p>人生好難，project 更難</p>
+		</div>
+	);
+};
+
+export default Introduction;
