ab9410d64af73ba1c391aaa494b49966dbb11273
KUAN-HSUN-LI <b06209027@ntu.edu.tw>
2019-06-30 01:08:37 +0800

    change position

--- server.js
+++ server.js
@@ -19,6 +19,16 @@ fs.exists('pdf_File', e => {
 // Bodyparser Middleware
 app.use(express.json());
 
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+	// Set static folder
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+
 // DB Config
 const db = config.get('mongoURI');
 
@@ -43,16 +53,6 @@ app.use('/api/users', require('./routes/api/users'));
 
 app.use('/api/utils', require('./routes/api/utils'));
 app.use('/api/answers', require('./routes/api/answers'));
-
-// Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-	// Set static folder
-	app.use(express.static('client/build'));
-
-	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-	});
-}
 
 const port = process.env.PORT || 5000;
 
